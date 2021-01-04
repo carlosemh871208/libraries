c4e0a74c7885315abf00eda568ca0ae44c2b6075
ingce <ingce@DESKTOP-URT7OER>
2021-01-04 14:58:26 -0600

    File name changed from stdtypes.h to Std_Types.h

---
+++ 000-Hello/src/libraries/Std_Types.h
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Description:
+ *
+*******************************************************************************/
+#ifndef LIBRARIES_STD_TYPES_H_
+#define LIBRARIES_STD_TYPES_H_
+/******************************************************************************/
+/***********************************Includes***********************************/
+
+/******************************************************************************/
+/***************************New types definitions******************************/
+/*Unsigned data types*/
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long  uint32_t;
+
+/*Signed data types*/
+typedef signed char    sint8_t;
+typedef signed short   sint16_t;
+typedef signed long    sint32_t;
+
+/*Boolean definition*/
+#ifndef boolean
+typedef uint8_t boolean;
+#endif
+
+/*RTOS task states definition*/
+#ifndef taskState
+typedef uint8_t taskState;
+#endif
+/******************************************************************************/
+/*****************************Macros definitions*******************************/
+/*Boolean macros*/
+#define FALSE ((uint8_t) 0U)
+#define TRUE  ((uint8_t) 1U)
+
+/*Main program macros*/
+#define EXIT_SUCCESS ((uint8_t) 0U)
+
+/*Read only variable macro*/
+#define __I  volatile const
+
+/*Write only variable macro*/
+#define __O  volatile
+
+/*Read / Write variable macro*/
+#define __IO volatile
+
+/*32 bits memory read macro*/
+#if !defined(REG_READ32)
+  #define REG_READ32(address)               (*(volatile uint32_t*)(address))
+#endif
+
+/*32 bits memory write macro*/
+#if !defined(REG_WRITE32)
+  #define REG_WRITE32(address, value)       ((*(volatile uint32_t*)(address))= (uint32_t)(value))
+#endif
+
+/*32 bits setting macro*/
+#if !defined(REG_BIT_SET32)
+  #define REG_BIT_SET32(address, mask)      ((*(volatile uint32_t*)(address))|= (uint32_t)(mask))
+#endif
+
+/*32 bits clearing macro*/
+#if !defined(REG_BIT_CLEAR32)
+  #define REG_BIT_CLEAR32(address, mask)    ((*(volatile uint32_t*)(address))&= ((uint32_t)~((uint32_t)(mask))))
+#endif
+
+/*RTOS task states*/
+/*The task which is executing currently is said to be in running state*/
+#define running   ((uint8_t) 0U)
+
+/*The task which is neither suspended nor blocked but still not executing will be ready state*/
+#define ready     ((uint8_t) 1U)
+
+/*A task will go in blocked state whenever its waiting for a event to happen*/
+#define blocked   ((uint8_t) 2U)
+
+/*The suspended tasks are also not available for scheduling*/
+#define suspended ((uint8_t) 3U)
+
+/******************************************************************************/
+
+
+/*******************************************************************************
+|   Author    |     Date    |                  Description                     |
+|-------------|-------------|--------------------------------------------------|
+|  Carlos M   | 21-Dec-2020 | First code version.                              |
+|  Carlos M   | 28-Dec-2020 | Added macros for register declaration            |
+*******************************************************************************/
+#endif /* LIBRARIES_STD_TYPES_H_ */
